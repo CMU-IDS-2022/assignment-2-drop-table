8f4477cc17406116a40efa5cebe99ac5379e02d9
Aaron Ho <aaronho@andrew.cmu.edu>
2022-02-14 14:46:00 -0500

    Adding new files and starting application file

--- application.py
+++ application.py
@@ -1,2 +1,9 @@
 import pandas as pd
 
+#df = pd.read_excel("EADA_2020.xlsx",engine = "openpyxl")
+df = pd.read_csv("health-raw-2021.csv",skiprows=3)
+#print(df)
+
+print(df.head(10))
+
+#print(df.columns)
