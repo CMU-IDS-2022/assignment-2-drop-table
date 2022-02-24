7d7ef66061cee1ed327286c8e9e7b7a38e0b4691
Aaron Ho <aaronho@andrew.cmu.edu>
2022-02-24 13:51:50 -0500

    Latest changes 2/24. Still need to fix one thing about a chart for more interactivity

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
+# Aaron Ho aaronho@andrew.cmu.edu
 from re import U
+from typing import Sized
+from matplotlib import colors
+from matplotlib.pyplot import title
 import streamlit as st
 import pandas as pd
 import altair as alt
+import numpy as np
 
 #Define functions to load data
-@st.cache
+# @st.cache(allow_output_mutation=True)
 def load_data():
     df = pd.read_csv("health-raw-2021.csv",skiprows=3)
     return df.sort_index()
@@ -18,30 +23,45 @@ def get_indicator_slice(indicator):
 st.title("Global Health")
 with st.spinner(text="Loading data..."):
     df = load_data()
-    df = df.loc[:,["Country Name",'Indicator Name','1960', '1961', '1962', '1963', '1964', '1965', '1966', '1967', '1968',
-'1969', '1970', '1971', '1972', '1973', '1974', '1975', '1976', '1977',
-'1978', '1979', '1980', '1981', '1982', '1983', '1984', '1985', '1986',
-'1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995',
-'1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004',
-'2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013',
-'2014', '2015', '2016', '2017', '2018', '2019', '2020']]
+    df.drop(columns=['Country Code','Indicator Code'],inplace=True)
     df = pd.melt(df,id_vars=["Country Name",'Indicator Name'])
     df.rename(columns = {"variable":'Year'},inplace=True)
+    # df = df.astype({'Year':'int64'})
+
 
 st.dataframe(df.head(1000))
+# eswatini = df[df["Country Name"]=="Eswatini"]
+# st.write(eswatini)
+
 
 st.header("Thinking of using the example in class where the research used a slider to show the plot points move like Hans Rosling and gapminder")
 
-#Define multi select to get indicators people can use
-potentials = df["Indicator Name"].unique()
-selection = st.multiselect("Select indicator(s)",default=potentials[0],options = potentials)
+#Define select to get indicators people can use
+potentials = [
+"Incidence of HIV, ages 15-49 (per 1,000 uninfected population ages 15-49)",
+"Incidence of HIV, ages 15-24 (per 1,000 uninfected population ages 15-24)",
+"Young people (ages 15-24) newly infected with HIV",
+"Incidence of HIV, all (per 1,000 uninfected population)",
+"Adults (ages 15+) and children (ages 0-14) newly infected with HIV",
+"Children (ages 0-14) newly infected with HIV",
+"Adults (ages 15-49) newly infected with HIV",
+"Prevalence of HIV, male (% ages 15-24)",
+"Prevalence of HIV, female (% ages 15-24)",
+"Children (0-14) living with HIV",
+"Prevalence of HIV, total (% of population ages 15-49)"
+]
+
+selection = st.selectbox("Select indicator(s)",options = potentials)
 
 #If selection is made. Only input to prevent errors
 if selection:
     #slice data based on indicators chosen
-    data = get_indicator_slice(selection)
-    data = data.dropna()
 
+    #Convert string to list 
+    selection = [selection]
+    selection.extend(['Antiretroviral therapy coverage (% of people living with HIV)',"Antiretroviral therapy coverage for PMTCT (% of pregnant women living with HIV)","Unmet need for contraception (% of married women ages 15-49)","Contraceptive prevalence, modern methods (% of women ages 15-49)","Current health expenditure (% of GDP)","Condom use, population ages 15-24, male (% of males ages 15-24)","Condom use, population ages 15-24, female (% of females ages 15-24)"])
+    data = get_indicator_slice(selection)
+    data.drop(data[data["Year"] == "Unnamed: 65"].index,inplace=True)
 
     #Get unique countries
     unique_countries = data["Country Name"].unique()
@@ -50,15 +70,89 @@ if selection:
     #If countries selected. Only input to prevent errors
     if countries:
         data =data[data["Country Name"].isin(countries)]
+        # st.write(data)
 
-        #Pivot data and clean it up
+        # #Pivot data and clean it up
+        data["value"] = data["value"].fillna(0)
         data = data.pivot_table(index=['Country Name',"Year"],columns="Indicator Name",values=['value'])
+        # st.write(data)
         data.columns = data.columns.get_level_values(1)
         data.reset_index(inplace=True)
         # data = data.dropna()
+        data = data.astype({'Year':'int32'})
 
-        #Write data to application
         st.write(data)
+        select_year = alt.selection_single(name='select', fields=['Year'], 
+        init={'Year': min(data["Year"])},bind=alt.binding_range(min=min(data["Year"]), max=max(data["Year"]), step=1.0))
+
+        #Allow users to select on a point to display something
+        single_country = alt.selection_single(name="select_country", fields=["Country Name"])
+        single_year = alt.selection_single(name="select_year",fields=["Year"])
+        
+
+         # st.write(single_year)
+        #Visual to display when clicked
+        #Repeating graphs to show related metrics to the indicator chosen
+        columns = ["Unmet need for contraception (% of married women ages 15-49)","Contraceptive prevalence, modern methods (% of women ages 15-49)","Current health expenditure (% of GDP)","Condom use, population ages 15-24, male (% of males ages 15-24)","Condom use, population ages 15-24, female (% of females ages 15-24)"] 
+        # rows = ["Country Name"]
+        #Add related repeating charts
+        related = alt.Chart(data).mark_bar().encode(
+                alt.X(alt.repeat("row"),type='quantitative'),
+                alt.Y(alt.repeat("column"),type='quantitative'),
+               alt.Color('Country Name'),
+               alt.Tooltip("Year")
+             ).repeat(
+                column=columns,
+                row=[selection[0]]
+            ).add_selection(single_country).transform_filter(single_country)
+        # text = related.mark_text(
+        #     #  align='left',
+        #     # baseline='middle',
+        #     # dx=3  # Nudges text to right so it doesn't appear on top of the bar
+        # ).encode(
+        #     text='Year:O'
+        # )
+# add_selection(single_country,single_year).
+        
+        #Left chart
+        left = alt.Chart(data,title="% of people living with HIV recieving antiretroviral therapy").mark_circle().encode(
+            alt.X(selection[0],type='quantitative'),
+            alt.Y(selection[1],title='Antiretroviral therapy coverage', type = "quantitative"),
+            alt.Size(selection[1]),
+            color=alt.condition(single_country,'Country Name' ,alt.value('lightgray'))
+        ).add_selection(select_year,single_country).transform_filter(select_year)
+
+        #Right Chart
+        right = alt.Chart(data,title='% of pregnant women living with HIV recieving antiretroviral therapy').mark_circle().encode(
+            alt.X(selection[0],type='quantitative'),
+            alt.Y(selection[2],title='Antiretroviral therapy coverage', type = "quantitative"),
+            alt.Size(selection[1]),
+            color=alt.condition(single_country,'Country Name' ,alt.value('lightgray')),
+        ).add_selection(select_year,single_country).transform_filter(select_year)
+        
+
+        st.altair_chart(left|right)
+
+        # st.write(single_year)
+        #Visual to display when clicked
+        #Repeating graphs to show related metrics to the indicator chosen
+        # columns = ["Unmet need for contraception (% of married women ages 15-49)","Contraceptive prevalence, modern methods (% of women ages 15-49)","Current health expenditure (% of GDP)","Condom use, population ages 15-24, male (% of males ages 15-24)","Condom use, population ages 15-24, female (% of females ages 15-24)"] 
+        # # rows = ["Country Name"]
+        # #Add related repeating charts
+        # related = alt.Chart(data).mark_bar().encode(
+        #         alt.X(alt.repeat("row"),type='quantitative'),
+        #         alt.Y(alt.repeat("column"),type='quantitative'),
+        #        alt.Color('Country Name'),
+        #        alt.Tooltip("Year")
+        #     ).repeat(
+        #         column=columns,
+        #         row=[selection[0]]
+        #     ).add_selection(single_country,single_year).transform_filter(single_country).transform_filter(single_year)
+
+
+# .transform_filter(single_country).transform_filter(single_year)
+        st.altair_chart(related)
+    
     else:
         st.write(data.head(50))
 else:
@@ -67,21 +161,4 @@ else:
 
 
 
-select_year = alt.selection_single(
-    name='select', fields=['year'], init={'year': 1955},
-    bind=alt.binding_range(min=1960, max=2020, step=1)
-)
 
-
-# pop = alt.Chart(data).mark_point(filled=True).encode(
-#     alt.X("Year", scale=alt.Scale(zero=False)),
-#     alt.Y("",scale=alt.Scale(zero=False)),
-#     # alt.Size('value:Q', scale=alt.Scale(domain=[0, 1200000000], range=[0,1000])),
-#     alt.Color('Country Name:N', legend=None),
-#     alt.OpacityValue(0.5),
-#     alt.Tooltip('value:Q')
-#     # alt.Order('pop:Q', sort='descending')
-# )
-# .add_selection(select_year).transform_filter(select_year)
-
-# st.altair_chart(pop)
